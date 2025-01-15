9018771bc454d954d8194b5359a7737a60a0fba6
Alex Minerva <jlptrenshuu@gmail.com>
2025-01-15 16:12:11 +0100

    fix error

--- WickedEngine/wiHelper.cpp
+++ WickedEngine/wiHelper.cpp
@@ -1342,7 +1342,6 @@ namespace wi::helper
 			MultiByteToWideChar(CP_UTF8, 0, from.c_str(), -1, &to[0], num);
 		}
 #else
-		// to = std::filesystem::path(from).wstring();
 		std::wstring_convert<std::codecvt_utf8<wchar_t>> cv;
 		to = cv.from_bytes(from);
 #endif // _WIN32
@@ -1358,7 +1357,6 @@ namespace wi::helper
 			WideCharToMultiByte(CP_UTF8, 0, from.c_str(), -1, &to[0], num, NULL, NULL);
 		}
 #else
-		// to = std::filesystem::path(from).u8string();
 		std::wstring_convert<std::codecvt_utf8<wchar_t>> cv;
 		to = cv.to_bytes(from);
 #endif // _WIN32
