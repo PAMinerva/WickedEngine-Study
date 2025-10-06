abd113a8521a2a45c649a1010572e5a345d3e18b
Alex P. Minerva <alex79.minerva@gmail.com>
2025-10-06 11:27:25 +0200

    10-SoftBodies clean code

--- Samples/My_Samples/Physics/Ten_Minute_Physics/10-SoftBodies/main_SDL2.cpp
+++ Samples/My_Samples/Physics/Ten_Minute_Physics/10-SoftBodies/main_SDL2.cpp
@@ -1,96 +1,93 @@
 #include "stdafx.h"
-#include "sample.h"
-
-#ifdef SDL2
-#include <SDL2/SDL.h>
+#include "sdl2.h"
 
 #define MAX_LOADSTRING 100
 
 // Global Variables:
-wi::Application application;					// Application class
+SampleApp app;
 
-int sdl_loop(wi::Application &application)
+int sdl_loop()
 {
-    SDL_Event event;
-
     bool quit = false;
     while (!quit)
     {
-        SDL_PumpEvents();
-        application.Run();
-
-        while( SDL_PollEvent(&event)) 
-        {
-            switch (event.type) 
-            {
-                case SDL_QUIT:      
+        app.Run();
+        SDL_Event event;
+        while(SDL_PollEvent(&event)){
+            switch(event.type){
+                case SDL_QUIT:
                     quit = true;
                     break;
                 case SDL_WINDOWEVENT:
-                    switch (event.window.event) 
-                    {
-                    case SDL_WINDOWEVENT_CLOSE:
-                        quit = true;
-                        break;
-                    case SDL_WINDOWEVENT_RESIZED:
-                        application.SetWindow(application.window);
-                        break;
-                    default:
-                        break;
+                    switch (event.window.event) {
+                        case SDL_WINDOWEVENT_CLOSE: // exit tests
+                            quit = true;
+                            break;
+                        case SDL_WINDOWEVENT_RESIZED:
+                            // Tells the engine to reload window configuration (size and dpi)
+                            app.SetWindow(app.window);
+                            break;
+                        case SDL_WINDOWEVENT_FOCUS_LOST: //TODO
+                            app.is_window_active = false;
+                            break;
+                        case SDL_WINDOWEVENT_FOCUS_GAINED:
+                            app.is_window_active = true;
+                            if (wi::shadercompiler::GetRegisteredShaderCount() > 0)
+                            {
+                                std::thread([] {
+                                    wi::backlog::post("[Shader check] Started checking " + std::to_string(wi::shadercompiler::GetRegisteredShaderCount()) + " registered shaders for changes...");
+                                    if (wi::shadercompiler::CheckRegisteredShadersOutdated())
+                                    {
+                                        wi::backlog::post("[Shader check] Changes detected, initiating reload...");
+                                        wi::eventhandler::Subscribe_Once(wi::eventhandler::EVENT_THREAD_SAFE_POINT, [](uint64_t userdata) {
+                                            wi::renderer::ReloadShaders();
+                                            });
+                                    }
+                                    else
+                                    {
+                                        wi::backlog::post("[Shader check] All up to date");
+                                    }
+                                    }).detach();
+                            }
+                            break;
+                        default:
+                            break;
                     }
                 default:
                     break;
             }
+            wi::input::sdlinput::ProcessEvent(event);
         }
     }
 
     return 0;
 }
 
-int main(const int argc, const char* argv[])
+int main(int argc, char* argv[])
 {
-    wi::Application application;
-    #ifdef WickedEngine_SHADER_DIR
-    wi::renderer::SetShaderSourcePath(WickedEngine_SHADER_DIR);
-    #endif
 
-    application.infoDisplay.active = true;
-    application.infoDisplay.watermark = true;
-    application.infoDisplay.resolution = true;
-    application.infoDisplay.fpsinfo = true;
-    application.infoDisplay.colorspace = true;
-    application.infoDisplay.device_name = true;
+  // TODO: Place code here.
+
+    wi::arguments::Parse(argc, argv);
 
     sdl2::sdlsystem_ptr_t system = sdl2::make_sdlsystem(SDL_INIT_EVERYTHING | SDL_INIT_EVENTS);
+    if (*system) {
+        throw sdl2::SDLError("Error creating SDL2 system");
+    }
+
     sdl2::window_ptr_t window = sdl2::make_window(
-            "Hello Triangle",
+            "Wicked Engine Tests",
             SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-            1920, 1080,
-            SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN | SDL_WINDOW_ALLOW_HIGHDPI);
+            1280, 800,
+            SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE);
+    if (!window) {
+        throw sdl2::SDLError("Error creating window");
+    }
 
-    SDL_Event event;
+    app.SetWindow(window.get());
 
-    application.SetWindow(window.get());
-
-    Sample sample;  // Sample class
-
-    struct DPI_INFO
-    {
-        float ddpi = 0;
-        float hdpi = 0;
-        float vdpi = 0;
-    } dpi_info;
-
-    SDL_GetDisplayDPI(0, &dpi_info.ddpi, &dpi_info.hdpi, &dpi_info.vdpi);
-    application.canvas.dpi = dpi_info.hdpi;
-
-    application.Initialize(); // Perform graphics initialization for the application (create device, swapchain, etc.)
-    application.ActivatePath(sample.GetRenderPath3D());
-
-    int ret = sdl_loop(application);
+    int ret = sdl_loop();
 
     SDL_Quit();
-
     return ret;
 }
-#endif
