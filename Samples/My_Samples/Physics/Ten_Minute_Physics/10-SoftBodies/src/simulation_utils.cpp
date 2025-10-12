83a90c41ed90dc53c86b3ab5b7d7638209026bf5
Alex P. Minerva <alex79.minerva@gmail.com>
2025-10-12 11:34:30 +0200

    new comments

--- Samples/My_Samples/Physics/Ten_Minute_Physics/10-SoftBodies/src/simulation_utils.cpp
+++ Samples/My_Samples/Physics/Ten_Minute_Physics/10-SoftBodies/src/simulation_utils.cpp
@@ -4,18 +4,21 @@
 #include "softBody.h"
 
 static wi::ecs::Entity mat_entity = wi::ecs::INVALID_ENTITY;
+static uint64_t shader_id = 0;
 
 simulation::PhysicsScene gPhysicsScene;
 wi::gui::Label label_tets;
 
 namespace simulation
 {
-	void init_physics()
+	void init_physics(uint64_t shaderID)
 	{
-		SoftBodyParams params{
-			.rotate = {0.0f, 0.0f, 2.7f},
-			.edgeCompliance = 100.0f,
-			.volCompliance = 0.0f};
+		shader_id = shaderID;
+
+		SoftBodyParams params{};
+		params.rotate = {0.0f, 0.0f, 3.14f}; // rotate 180 degrees around Z axis clockwise (180 x (pi/180) = pi = 3.14 rad)
+		params.edgeCompliance = 100.0f;
+		params.volCompliance = 0.0f;
 
 		auto instance = create_softbody_instance(params);
 		int numTets = instance->softBody->numTets;
@@ -39,7 +42,8 @@ namespace simulation
 			for (auto &instance : gPhysicsScene.objects)
 				instance->softBody->PreSolve(subdt, gPhysicsScene.gravity);
 
-			// 2. Solve: constraints (edge, volume, etc.)
+			// 2. Solve: project constraints (edge, volume, etc.)
+			// Projecting a set of points according to a constraint means moving (correcting) the points such that they satisfy the constraint
 			for (auto &instance : gPhysicsScene.objects)
 				instance->softBody->Solve(subdt);
 
@@ -61,10 +65,11 @@ namespace simulation
 		std::uniform_real_distribution<float> dist_y(0, a);
 		std::uniform_real_distribution<float> dist_z(0, a);
 
-		SoftBodyParams params{
-			.translate = {dist_x(gen), dist_y(gen), dist_z(gen)},
-			.edgeCompliance = 100.0f,
-			.volCompliance = 0.0f};
+		SoftBodyParams params{};
+		params.translate = {dist_x(gen), dist_y(gen), dist_z(gen)};
+		params.edgeCompliance = 100.0f;
+		params.volCompliance = 0.0f;
+
 		auto instance = create_softbody_instance(params);
 		gPhysicsScene.objects.push_back(std::move(instance));
 
@@ -81,11 +86,11 @@ namespace simulation
 					wi::scene::MeshComponent &mesh,
 					bool updateGPUBuffer)
 	{
-		// Update the vertex positions of the mesh triangle by triangle
+		// Update the vertex positions of the mesh, triangle by triangle
 		size_t numTris = bunnyMesh.tetSurfaceTriIds.size() / 3;
-		for (size_t t = 0; t < numTris; ++t)
+		for (size_t t = 0; t < numTris; ++t) // for each surface triangle...
 		{
-			// Indices of the vertices for this triangle
+			// Indices of the vertices for the current triangle
 			uint32_t i0 = bunnyMesh.tetSurfaceTriIds[t * 3 + 0];
 			uint32_t i1 = bunnyMesh.tetSurfaceTriIds[t * 3 + 1];
 			uint32_t i2 = bunnyMesh.tetSurfaceTriIds[t * 3 + 2];
@@ -99,7 +104,7 @@ namespace simulation
 			XMVECTOR v0 = XMLoadFloat3(&p0);
 			XMVECTOR v1 = XMLoadFloat3(&p1);
 			XMVECTOR v2 = XMLoadFloat3(&p2);
-			XMVECTOR n = XMVector3Cross(v2 - v0, v1 - v0);
+			XMVECTOR n = XMVector3Cross(v1 - v0, v2 - v0);
 			n = XMVector3Normalize(n);
 
 			XMFLOAT3 fn;
@@ -136,7 +141,7 @@ namespace simulation
 		if (!updateGPUBuffer)
 			return;
 
-		// Update the GPU buffer where are the vertex positions and normals of the mesh
+		// Update the GPU buffer where are stored the vertex positions and normals of the mesh
 		auto device = wi::graphics::GetDevice();
 		auto cmd = device->BeginCommandList();
 		if (mesh.position_format == wi::scene::MeshComponent::Vertex_POS16::FORMAT)
@@ -198,7 +203,7 @@ namespace simulation
 							mesh.vb_nor.offset);
 	}
 
-std::unique_ptr<SoftBodyInstance> create_softbody_instance(const SoftBodyParams &params)
+	std::unique_ptr<SoftBodyInstance> create_softbody_instance(const SoftBodyParams &params)
 	{
 		using namespace wi::scene;
 		using namespace wi::ecs;
@@ -211,7 +216,8 @@ std::unique_ptr<SoftBodyInstance> create_softbody_instance(const SoftBodyParams 
 			MaterialComponent &material = *GetScene().materials.GetComponent(mat_entity);
 			material.shaderType = MaterialComponent::SHADERTYPE_PBR;
 			material.baseColor = XMFLOAT4(0.941f, 0.125f, 0.0f, 1.0f);
-			material.SetDoubleSided(true);
+			// material.SetDoubleSided(true);
+			material.SetCustomShaderID(shader_id);
 		}
 
 		// Create a new entity.
@@ -219,18 +225,25 @@ std::unique_ptr<SoftBodyInstance> create_softbody_instance(const SoftBodyParams 
 		Entity entity = CreateEntity();
 
 		GetScene().layers.Create(entity);
-		GetScene().transforms.Create(entity);
+		GetScene().transforms.Create(entity); // Transform component is required for applying translations, rotations, scalings, etc. to the object
+
+		// A mesh component stores the mesh data (vertices, indices, subsets, etc.).
+		// An object component represents a renderable object in the scene and holds the mesh component entity that describes its geometry.
+        // Note that in this sample we compute the physics simulation on soft body mesh data and use it to update data in the
+        // mesh component. So we cannot use multiple instance object sharing the same mesh component (we need a mesh per object).
 		ObjectComponent &object = GetScene().objects.Create(entity);
 		MeshComponent &mesh = GetScene().meshes.Create(entity);
 		object.meshID = entity;
 
-		// A subset specifies a range of indices to describe part of the mesh to be rendered with a specific material.
+		// A subset specifies a range of indices to describe part or whole
+		// of the mesh to be rendered with a specific material.
 		mesh.subsets.push_back(wi::scene::MeshComponent::MeshSubset());
 		mesh.subsets.back().materialID = mat_entity;
 		mesh.subsets.back().indexOffset = 0;
-		mesh.subsets.back().indexCount = (uint32_t)bunnyMesh.tetSurfaceTriIds.size();
+		mesh.subsets.back().indexCount = (uint32_t)bunnyMesh.tetSurfaceTriIds.size(); // tetSurfaceTriIds contains indices of surface triangles only
 
-		// We are only intrested in surface triangles of the tetrahedral mesh for rendering purposes
+		// Compute the number of surface triangles:
+		// For rendering purposes, we are only interested in surface triangles of the tetrahedral mesh.
 		size_t numTris = bunnyMesh.tetSurfaceTriIds.size() / 3;
 		mesh.vertex_positions.reserve(numTris * 3);
 		mesh.vertex_normals.reserve(numTris * 3);
@@ -239,7 +252,7 @@ std::unique_ptr<SoftBodyInstance> create_softbody_instance(const SoftBodyParams 
 		// For each surface triangle of the tetrahedral mesh
 		for (size_t t = 0; t < numTris; ++t)
 		{
-			// Indices of the vertices for this triangle
+			// Indices of the vertices for this triangle in clocwise order
 			uint32_t i0 = bunnyMesh.tetSurfaceTriIds[t * 3 + 0];
 			uint32_t i1 = bunnyMesh.tetSurfaceTriIds[t * 3 + 1];
 			uint32_t i2 = bunnyMesh.tetSurfaceTriIds[t * 3 + 2];
@@ -261,13 +274,18 @@ std::unique_ptr<SoftBodyInstance> create_softbody_instance(const SoftBodyParams 
 			XMVECTOR v0 = XMLoadFloat3(&p0);
 			XMVECTOR v1 = XMLoadFloat3(&p1);
 			XMVECTOR v2 = XMLoadFloat3(&p2);
-			XMVECTOR n = XMVector3Cross(v2 - v0, v1 - v0);
+			// We are in a left-handed coordinate system, so the vector resulting from the cross product
+			// "sees" the first vector rotated clockwise towards the second vector.
+			// In this case, the vertices of each triangle are given in clockwise order, so the first
+			// and second vectors will be, respectively, (v1 - v0) and (v2 - v0).
+			XMVECTOR n = XMVector3Cross(v1 - v0, v2 - v0);
 			n = XMVector3Normalize(n);
 
 			XMFLOAT3 fn;
 			XMStoreFloat3(&fn, n);
 
-			// Add the three vertices of this triangle to the vertex_position array of the mesh component
+			// Add the three vertices of this triangle to the vertex_position array of the mesh component.
+			// Remember that we are only storing vertices of surface triangles.
 			uint32_t baseIndex = (uint32_t)mesh.vertex_positions.size();
 			mesh.vertex_positions.push_back(p0);
 			mesh.vertex_positions.push_back(p1);
@@ -279,7 +297,7 @@ std::unique_ptr<SoftBodyInstance> create_softbody_instance(const SoftBodyParams 
 			mesh.vertex_normals.push_back(fn);
 
 			// Add the three vertex indices of this triangle to the index array of the mesh component.
-			// The indices are relative to the vertex_position array.
+			// Note that these indices are relative to the vertex_position array.
 			mesh.indices.push_back(baseIndex + 0);
 			mesh.indices.push_back(baseIndex + 1);
 			mesh.indices.push_back(baseIndex + 2);
@@ -287,6 +305,8 @@ std::unique_ptr<SoftBodyInstance> create_softbody_instance(const SoftBodyParams 
 
 		// Create a new SoftBodyInstance object that will contain the SoftBody object and the entity
 		// we will use to access the mesh and other components associated with this soft body object.
+		// SoftBody will store and manage all physics-related data (some taken from bunnyMesh) and operations
+		// (vertex positions will include all vertices of the tetrahedral mesh, not only surface vertices).
 		auto instance = std::make_unique<SoftBodyInstance>();
 		instance->softBody = std::make_unique<SoftBody>(bunnyMesh, params.edgeCompliance, params.volCompliance);
 		instance->entity = entity;
@@ -299,10 +319,12 @@ std::unique_ptr<SoftBodyInstance> create_softbody_instance(const SoftBodyParams 
 		// Initialize the physics-related data structures of the SoftBody object
 		instance->softBody->InitPhysics();
 
-		// Upload the mesh data to a buffer created as a sub-resource of a general GPU buffer
-		// containing all mesh data of the scene.
+		// Update mesh data according to the current state of the SoftBody object.
 		simulation::update_mesh(*instance->softBody, mesh, false);
 
+		// Upload the mesh data to a buffer created as a sub-resource of a general GPU buffer
+		// containing all mesh data of the scene, if available.
+		// Otherwise, create a new GPU buffer for this mesh only.
 		mesh.CreateRenderData();
 
 		return instance;
