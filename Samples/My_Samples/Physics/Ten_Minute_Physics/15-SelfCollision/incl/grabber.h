161da4d666dbefd6f3e18157c2c4220c5fce75e7
Alex P. Minerva <alex79.minerva@gmail.com>
2026-02-21 12:34:01 +0100

    Add 15-SelfCollision

---
+++ Samples/My_Samples/Physics/Ten_Minute_Physics/15-SelfCollision/incl/grabber.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "cloth.h"
+#include "wiScene.h"
+
+class Grabber
+{
+public:
+    void start(wi::scene::PickResult pick, ClothMesh* clothMesh);
+    void move(wi::primitive::Ray ray, wi::scene::PickResult pick);
+    void end();
+
+	void increaseTime(float dt);
+
+    wi::scene::PickResult pickInfo {};
+	ClothMesh* clothMesh = nullptr;
+    std::vector<float> prevPos = {0, 0, 0};
+    std::vector<float> vel = {0, 0, 0};
+    float time = 0.0f;
+    bool isGrabbing = false;
+	float startDistance = 0.0f;
+};
